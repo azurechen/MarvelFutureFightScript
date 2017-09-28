056284921ee00700cb06bc411efd5b49cd87dae2
azurechen <azure517981@gmail.com>
2017-09-28 23:36:45 +0800

    Update the cowork script to improve the performance.

--- auto_cowork.py
+++ auto_cowork.py
@@ -8,18 +8,26 @@ import atx
 d = atx.connect(os.getenv("SERIAL"))
 d.resolution = (1920, 1080)
 
-# team game
 while True:
-    # enter the lobby and choose a hero
-    if d.exists("imgs/lobby.1920x1080.png"):
+    d.keep_screen()
+    
+    # try next
+    if d.click_exists("imgs/next.1920x1080.png") != None:
+        d.delay(10.0)
+    # enter the lobby
+    elif d.exists("imgs/lobby.1920x1080.png"):
         d.delay(1.0)
-        if d.exists("imgs/get_award.1920x1080.png"):
-            d.click_image("imgs/get_award.1920x1080.png")
-            d.click_image("imgs/fetch.1920x1080.png", timeout=10.0)
-            d.click_image("imgs/yes.1920x1080.png", timeout=10.0)
+        d.free_screen()
+        # get awards
+        if d.click_exists("imgs/get_award.1920x1080.png") != None:
+            d.click_image("imgs/fetch.1920x1080.png", timeout=10.0, safe=True)
+            d.click_image("imgs/yes.1920x1080.png", timeout=10.0, safe=True)
+        # choose a hero and start
         else:
             d.click(118, 900)
-            d.click_image("imgs/match_start.1920x1080.png", timeout=10.0)
+            d.click_image("imgs/match_start.1920x1080.png", timeout=10.0, safe=True)
             d.delay(30.0)
-            d.click_image("imgs/next.1920x1080.png", timeout=60.0)
+    
+    # wait and play again
+    d.free_screen()
     d.delay(5.0)
